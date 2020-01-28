b62d57744bdbecb279daf6308af341523e6ca31b
FIXED-TERM Grossmann Tim (ETAS-SEC/ECT-Fe2) <grt1pal@bosch.com>
2020-01-28 14:07:49 +0100

    add subscription based auth
    Signed-off-by: FIXED-TERM Grossmann Tim (ETAS-SEC/ECT-Fe2) <grt1pal@bosch.com>

--- poc/azure_auth.py
+++ poc/azure_auth.py
@@ -7,12 +7,13 @@ SESSION = requests.Session()
 # client id and secret from application (has to be assigned to the subscription)
 token_response = auth_context.acquire_token_with_client_credentials(
     client_id='9efa748e-d6ca-4476-9048-7ee22bbfdbc5', 
-    client_secret='xZeld/bTYe8N:wJR]5ACw.LzeCJTrz21', 
+    client_secret='KPr02gMuz18zKVzf]laieTzyLpUtZ-?/', 
     resource='https://management.azure.com/')
 
 SESSION.headers.update({'Authorization': "Bearer " + token_response['accessToken']})
 
-policy_endpoint = 'https://management.azure.com/subscriptions/3a8b6402-45a2-4b9b-b6f7-73cbe8e507e2/providers/Microsoft.Authorization/policyDefinitions?api-version=2019-09-01'
+sub_policy_endpoint = 'https://management.azure.com/subscriptions/3a8b6402-45a2-4b9b-b6f7-73cbe8e507e2/providers/Microsoft.Authorization/policyDefinitions?api-version=2019-09-01'
+mgmt_policy_endpoint = 'https://management.azure.com/providers/Microsoft.Management/managementgroups/mgmt-test/providers/Microsoft.Authorization/policyAssignments?%24filter=atScope()&api-version=2019-09-01'
 
-json_out = SESSION.get(policy_endpoint).json()
+json_out = SESSION.get(mgmt_policy_endpoint).json()
 print(json_out)
